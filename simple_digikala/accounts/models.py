0f9e29f121d45778996198c8db054db7a09da699
parsamhb82 <parsa1382mohebian@gmail.com>
2024-08-17 21:21:25 +0330

    models initialized

---
+++ simple_digikala/accounts/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Seller(models.Model):
+    name = models.CharField(max_length = 50, verbose_name = 'seller_name', help_text = 'seller name should be less than 50 characters',blank = True, null = True)
+    city = models.CharField(max_length = 30, verbose_name = 'city_name', help_text = 'cities name should be less than 30 characters', blank = True, null = True)
+    
+    def __str__(self) -> str:
+        return self.name
+
+class Costumer(models.Model):
+    name = models.CharField(max_length = 50, verbose_name = 'costumer_name', help_text = 'costumer name should be less than 50 characters', blank = True, null = True)
+    city = models.CharField(max_length = 30, verbose_name = 'city_name', help_text = 'cities name should be less than 30 characters', blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.name
---
+++ simple_digikala/order_app/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from accounts.models import Costumer, Seller
+from product_app.models import Product
+
+
+
+class Discount(models.Model):
+    active = models.BooleanField(default=True, blank=True, null=True)
+    buyer = models.ForeignKey(Costumer, blank=True, null=True)
+    code = models.CharField(max_length=10, help_text='code shouldnt be more than 10 characters', blank=True, null=True)
+    initiated_date = models.DateField(blank=True, null=True)
+    expiry_date = models.DateField(blank=True, null=True)
+    percentage = models.IntegerField(blank=True, null=True)
+
+    
+
+class Order(models.Model):
+    buyer = models.ForeignKey(Costumer, on_delete = models.CASCADE,blank=True, null=True)
+    seller = models.ForeignKey(Seller, on_delete= models.CASCADE,blank=True, null=True)
+    transaction_status = models.BooleanField(blank=True, null=True)
+    date = models.DateField(help_text='the time when the order was set first', blank=True, null=True)
+    discount_code = models.ForeignKey(Discount, on_delete = models.PROTECT, blank=True, null=True)
+    bill = models.FloatField(blank=True, null=True)
+
+class Order_item(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, help_text = 'this item is related to which order', blank=True, null=True)
+    product = models.ForeignKey(Product, on_delete= models.CASCADE, help_text='this product', blank=True, null=True)
+    price = models.FloatField(blank=True, null=True)
+    num = models.IntegerField(blank= True, null=True)
+
+
+
+    
